e33642db52327251ebb8a673ef98e5849431309e
adrian <devxsolutionsit@gmail.com>
2025-07-28 18:09:59 -0500

    Fix Next.js build errors, successful local compilation

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,31 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-    ],
+    domains: ['dmg-cms-backend.onrender.com'], // <--- ¡Añade el dominio de tu backend aquí!
   },
+  // Opcional: Configuración para alias si usas @/
+  // webpack(config) {
+  //   config.resolve.alias['@'] = require('path').join(__dirname, 'src');
+  //   return config;
+  // },
 };
 
 export default nextConfig;
+
+
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '1337',
+//         pathname: '/uploads/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
